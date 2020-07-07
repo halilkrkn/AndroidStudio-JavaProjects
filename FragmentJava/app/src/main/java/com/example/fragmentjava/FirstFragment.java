d5ccbfcb8841fba95d5aedf0360a909fb1cd4abc
Halil Karkın <halilkrkn000@gmail.com>
2020-07-07 14:43:56 +0300

    Fragment Learned

---
+++ FragmentJava/app/src/main/java/com/example/fragmentjava/FirstFragment.java
@@ -0,0 +1,23 @@
+package com.example.fragmentjava;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.first_fragment,container,false);
+        TextView textView = viewGroup.findViewById(R.id.textview);
+        textView.setText("1st Fragment Example");
+        return viewGroup;
+    }
+}
--- FragmentJava/app/src/main/java/com/example/fragmentjava/MainActivity.java
+++ FragmentJava/app/src/main/java/com/example/fragmentjava/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fragmentjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -13,9 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void goToFirst(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frame_layout,firstFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout,firstFragment).commit();
+        //fragmentTransaction.commit();
 
     }
     public void goToSecond(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        SecondFragment secondFragment = new SecondFragment();
+        fragmentTransaction.replace(R.id.frame_layout,secondFragment).commit();
+        //fragmentTransaction.commit();
     }
 }
