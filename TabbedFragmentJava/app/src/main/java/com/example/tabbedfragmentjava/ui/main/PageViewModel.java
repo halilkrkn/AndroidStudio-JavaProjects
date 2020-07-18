1f1c3edf30bbdb57c42c5a0a1f1cea2897224bf1
Halil Karkın <halilkrkn000@gmail.com>
2020-07-18 15:47:04 +0300

    tabbed Fargment learned

--- TabbedFragmentJava/app/src/main/java/com/example/tabbedfragmentjava/ui/main/PageViewModel.java
+++ TabbedFragmentJava/app/src/main/java/com/example/tabbedfragmentjava/ui/main/PageViewModel.java
@@ -8,21 +8,11 @@ import androidx.lifecycle.ViewModel;
 
 public class PageViewModel extends ViewModel {
 
-    private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
-    private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
-        @Override
-        public String apply(Integer input) {
-            return "Hello world from section: " + input;
-        }
-    });
-
-    public void setIndex(int index) {
-
-        mIndex.setValue(index);
-    }
-
-    public LiveData<String> getText()
-    {
-        return mText;
-    }
+  private MutableLiveData<String> nameInput = new MutableLiveData<>();
+  public void setName (String name){
+      nameInput.setValue(name);
+  }
+  public LiveData<String> getName(){
+      return nameInput;
+  }
 }
